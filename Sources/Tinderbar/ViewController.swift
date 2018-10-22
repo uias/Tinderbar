833c2586208f03e676328e7c1d3bdaf04151aea6
Merrick Sapsford <merrick@sapsford.tech>
2018-10-22 19:27:21 +0200

    Add project

---
+++ Sources/Tinderbar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinderbar
+//
+//  Created by Merrick Sapsford on 22/10/2018.
+//  Copyright © 2018 UI At Six. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
